71dea9f2606cd4ffd5041155b567074e6e50e63d
zawad17 <abdzawad00007@gmail.com>
2025-08-21 02:44:08 +0600

    Hello World

---
+++ 01.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Zawad!")
+	fmt.Println(subtract(10, 5))
+	fmt.Println(add(3, 5))
+}
+func add(a int, b int) int {
+	return a + b
+}
+func subtract(a int, b int) int {
+	return a - b
+}
