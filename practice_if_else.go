36675e821c9f3baa94a0c6cfa10633f4e7356440
zawad17 <abdzawad00007@gmail.com>
2025-08-26 03:40:14 +0600

    basic_level01,02

---
+++ practice_if_else.go
@@ -0,0 +1,31 @@
+package main
+import "fmt"
+func main(){
+
+	//Basic level 01 if else condition
+
+	/*
+	var number int 
+	fmt.Println("Enter a number:")
+	fmt.Scanln(&number)
+	if number % 2== 0{
+		fmt.Println("The number is even")
+	}else{
+		fmt.Println("The number is odd")
+	}
+	*/
+
+	//Basic level 02 if else condition
+	var username string
+	var password string
+	fmt.Println("Enter your username:")
+	fmt.Scanln(&username)
+	fmt.Println("Enter your password: ")
+	fmt.Scanln(&password)
+	if username =="admin" && password == "12345abc"{
+		fmt.Println("Login Successful")
+	}else{
+		fmt.Println("Login Failed")
+	}
+
+}
