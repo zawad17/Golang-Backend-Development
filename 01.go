66aea4e999a60c515fbbb8e5e95b090a94b45c7f
zawad17 <abdzawad00007@gmail.com>
2025-08-26 02:58:37 +0600

    basic updated

--- 01.go
+++ 01.go
@@ -6,6 +6,8 @@ func main() {
 	fmt.Println("Hello, Zawad!")
 	fmt.Println(subtract(10, 5))
 	fmt.Println(add(3, 5))
+	fmt.Println(add(3, 5))
+
 
 }
 func add(a int, b int) int {
@@ -13,4 +15,5 @@ func add(a int, b int) int {
 }
 func subtract(a int, b int) int {
 	return a - b
+
 }
