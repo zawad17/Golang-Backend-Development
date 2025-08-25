bbee2fb54b7f31ef30f31ce80c7c109854256dca
zawad17 <abdzawad00007@gmail.com>
2025-08-26 02:47:55 +0600

    variable

---
+++ 02.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Hello, Zawad!")
+	/*
+
+		variable declaration
+
+	*/
+	var a int = 10
+	var b = 20
+	c := 20
+	fmt.Println(a, b, c)
+	fmt.Println(a + b + c)
+	d := true 
+	fmt.Println(d)
+	d = false 
+	fmt.Println(d)
+	
+}
